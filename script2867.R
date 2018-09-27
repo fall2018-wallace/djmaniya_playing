0e669081414e02f22252e169a5890849de5127cb
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-09-27 13:12:48 -0400

    Saving script2867.R

--- script2867.R
+++ script2867.R
@@ -4,7 +4,7 @@ df <- df
 df <- na.omit(df)
 head(df)
 plot <- hist(df$Ozone)
-trial <- lm(Temp ~ Ozone + Solar.R + Wind,data = aq)
+trial <- lm(Temp ~ Ozone + Solar.R + Wind,data = df)
 trial1 <- summary(trial)
 trial1 
 plot
